d27754d6618f1359ec39aace70e0f2bf3a78a131
NitayMagal0 <nmagal@g.jct.ac.il>
2024-11-05 10:40:53 +0200

    Initial Code + Welcome functions

---
+++ Stage0/Program5864.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Stage0
+{
+    partial class Program
+    {
+        private static void Main(string[] args)
+        {
+            welcome5864();
+            Welcome8145();
+            Console.ReadKey();
+        }
+
+        static partial void Welcome8145();
+
+        private static void welcome5864()
+        {
+            Console.WriteLine("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console aplication", name);
+        }
+
+}
+
+}
