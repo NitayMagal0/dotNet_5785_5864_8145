a83975ac89c16c2a85c789877c5fe4ba8dffa6e2
OfekOded <ofeko@yatmal.co.il>
2024-11-10 17:18:16 +0200

    I started chapter 4 defining a configuration entity

---
+++ DalList/Config.cs
@@ -0,0 +1,10 @@
+﻿namespace Dal;
+
+internal static class Config
+{
+    internal const int startCourseId = 1000;
+    private static int nextCourseId = startCourseId;
+    internal static int NextCourseId { get => nextCourseId++; }
+
+    internal static DateTime Clock { get; set; } = DateTime.Now;
+}
