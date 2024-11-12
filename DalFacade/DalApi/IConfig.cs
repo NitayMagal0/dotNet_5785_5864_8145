5ae290ee1f0d62dab2b5f0c5223aa81054c4def3
NitayMagal0 <nmagal@g.jct.ac.il>
2024-11-12 10:59:21 +0200

    Implemented VolunteerImplemetation (with throwing exeptions)

--- DalFacade/DalApi/IConfig.cs
+++ DalFacade/DalApi/IConfig.cs
@@ -1,10 +1,20 @@
-﻿
-namespace DalApi;
+﻿namespace DalApi;
 
-
-public interface IConfig
+public interface IConfig  
 {
-    DateTime Clock { get; set; }
+    internal const int startCallId = 1000;
+    private static int nextCallId = startCallId;
+    internal static int NextCallId { get => nextCallId++; }
+
+    internal const int startAssignmentId = 1000;
+    private static int nextAssignmentId = startAssignmentId;
+    internal static int NextAssignmentId { get => nextAssignmentId++; }
+
+    internal static DateTime Clock { get; set; } = DateTime.Now;
+
+    internal static TimeSpan RiskRange { get; set; } = TimeSpan.Zero;
+
+    //DateTime Clock { get; set; }
     //...
     void Reset();
 }
--- DalList/VolunteerImplementation.cs
+++ DalList/VolunteerImplementation.cs
@@ -5,33 +5,78 @@ using System.Collections.Generic;
 
 public class VolunteerImplementation : IVolunteer
 {
+    /// <summary>
+    /// Adds a new volunteer to the data source.
+    /// </summary>
+    /// <param name="item">The <see cref="Volunteer"/> object to add.</param>
+    /// <exception cref="Exception">
+    /// Thrown if a volunteer with the same ID already exists in the data source.
+    /// </exception>
     public void Create(Volunteer item)
     {
-        throw new NotImplementedException();
+        if (Read(item.Id) is not null)
+            throw new Exception($"Volunteer with ID={item.Id} already exists");
+
+        DataSource.Volunteers.Add(item);
     }
 
+
+    /// <summary>
+    /// Deletes a volunteer by their ID.
+    /// </summary>
+    /// <param name="id">The ID of the volunteer to delete</param>
+    /// <exception cref="Exception">Thrown if no volunteer with the specified ID is found.</exception>
     public void Delete(int id)
     {
-        throw new NotImplementedException();
+        Volunteer temp = Read(id);
+        if (temp is not null)
+            throw new Exception($"Volunteer with ID={id} doesn't exist");
+        DataSource.Volunteers.Remove(temp);
     }
 
+    /// <summary>
+    /// Deletes all the volunteers
+    /// </summary>
     public void DeleteAll()
     {
-        throw new NotImplementedException();
+        DataSource.Volunteers.Clear();
     }
 
+    /// <summary>
+    /// Retrieves a volunteer by their ID.
+    /// </summary>
+    /// <param name="id">The ID of the volunteer to retrieve.</param>
+    /// <returns>The volunteer with the specified ID.</returns>
+    /// <exception cref="KeyNotFoundException">Thrown if no volunteer with the specified ID is found.</exception>
     public Volunteer? Read(int id)
     {
-        throw new NotImplementedException();
+        var volunteer = DataSource.Volunteers.FirstOrDefault(v => v.Id == id);
+
+        if (volunteer == null)
+        {
+            throw new KeyNotFoundException($"Volunteer with ID {id} not found.");
+        }
+
+        return volunteer;
     }
 
+    /// <summary>
+    /// Retrieves all volunteers from the data source.
+    /// </summary>
+    /// <returns>A list of all <see cref="Volunteer"/> objects.</returns>
     public List<Volunteer> ReadAll()
     {
-        throw new NotImplementedException();
+        return DataSource.Volunteers;
     }
 
+
+    /// <summary>
+    /// Updates the details of an existing volunteer by first deleting the current record and then creating a new record with the updated details.
+    /// </summary>
+    /// <param name="item">object containing the updated details of the volunteer</param>
     public void Update(Volunteer item)
     {
-        throw new NotImplementedException();
+        Delete(item.Id);
+        Create(item);
     }
 }
