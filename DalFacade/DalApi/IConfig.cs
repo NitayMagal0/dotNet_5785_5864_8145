7803b6b1d1db01f2f8bbd35ad64642a0a3e914e8
OfekOded <ofeko@yatmal.co.il>
2024-11-12 11:51:25 +0200

    A combination of the two projects, a second attempt

--- DalFacade/DalApi/IConfig.cs
+++ DalFacade/DalApi/IConfig.cs
@@ -1,15 +1,14 @@
-﻿namespace Dal
+﻿namespace DalApi;
+
+public interface IConfig
 {
-    public interface IConfig
-    {
-        int NextCallId { get; } //Gets the next CallId.
+    int NextCallId { get; } //Gets the next CallId.
 
-        int NextAssignmentId { get; } //Gets the next AssignmentId.
+    int NextAssignmentId { get; } //Gets the next AssignmentId.
 
-        DateTime Clock { get; set; } //Gets or sets the current system time.
+    DateTime Clock { get; set; } //Gets or sets the current system time.
 
-        TimeSpan RiskRange { get; set; } //Gets or sets the risk range for the current assignment.
+    TimeSpan RiskRange { get; set; } //Gets or sets the risk range for the current assignment.
 
-        void Reset(); //Resets the configuration to its default state.
-    }
+    void Reset(); //Resets the configuration to its default state.
 }
