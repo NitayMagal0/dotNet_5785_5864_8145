e2e4e30da832bd56bdc49ad0f835002ddbe7440d
OfekOded <ofeko@yatmal.co.il>
2024-11-13 12:36:28 +0200

    I made an improvement to the code I wrote last time (implementation of the Assignment interface)

--- DalList/AssignmentImplementation.cs
+++ DalList/AssignmentImplementation.cs
@@ -10,11 +10,15 @@ public class AssignmentImplementation : IAssignment
         Assignment newAssignment = item with { Id = Config.NextAssignmentId };
         DataSource.Assignments.Add(newAssignment);
     }
-
+    /// <summary>
+    /// This function performs the deletion operation if the item exists otherwise throws an error
+    /// </summary>
+    /// <param name="id">The Id of the item to be deleted</param>
+    /// <exception cref="InvalidOperationException">Error that the user tried to perform an illegal operation (delete an item that does not exist in the database)</exception>
     public void Delete(int id)
     {
         Assignment? Assignment = Read(id);
-        if (Assignment != null)
+        if (Assignment is not null)   // Makes sure the item to delete exists
         {
             DataSource.Assignments.Remove(Assignment);
         }
@@ -23,7 +27,9 @@ public class AssignmentImplementation : IAssignment
             throw new InvalidOperationException($"Object of type Assignment with ID {id} does not exist.");
         }
     }
-
+    /// <summary>
+    /// This function deletes the entire buffer by running the remove function on each of the items
+    /// </summary>
     public void DeleteAll()
     {
         foreach (Assignment item in DataSource.Assignments)
@@ -41,15 +47,19 @@ public class AssignmentImplementation : IAssignment
     {
         return new List<Assignment>(DataSource.Assignments);
     }
-
+    /// <summary>
+    /// This function is responsible for updating the database if the item to be updated exists, if it does not exist it will throw an error
+    /// </summary>
+    /// <param name="item">The item you want to update</param>
+    /// <exception cref="InvalidOperationException">Error that the user tried to do an illegal operation (update an item that does not exist)</exception>
     public void Update(Assignment item)
     {
         Assignment? unupdatedAssignment = Read(item.Id);
-        if (unupdatedAssignment == null)
+        if (unupdatedAssignment is not null)   // if unupdatedAssignment is null it means the item doe's not exists
             throw new InvalidOperationException($"Object of type Assignment with ID {item.Id} does not exist.");
         else
         {
-            Delete(unupdatedAssignment.Id);
+            Delete(unupdatedAssignment!.Id);
             Create(item);
         }
     }
