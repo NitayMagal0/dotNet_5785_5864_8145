168a5333b07e893918f6f67d4d6779300257b4ed
NitayMagal0 <nmagal@g.jct.ac.il>
2024-11-26 11:25:48 +0200

    added xml filles, classes and more, currently Working on 4a

---
+++ DalXml/Config.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dal
+{
+    internal static class Config
+    {
+        internal const string s_data_config_xml = "data-config.xml";
+        internal const string s_volunteers_xml = "volunteers.xml";
+        internal const string s_calls_xml = "calls.xml";
+        internal const string s_assignments_xml = "assignments.xml";
+
+        internal static DateTime Clock
+        {
+            get => XMLTools.GetConfigDateVal(s_data_config_xml, "Clock");
+            set => XMLTools.SetConfigDateVal(s_data_config_xml, "Clock", value);
+        }
+
+        internal static int NextCallId
+        {
+            get => XMLTools.GetAndIncreaseConfigIntVal(s_calls_xml, "NextCallId");
+            private set =>XMLTools.SetConfigIntVal(s_calls_xml, "NextCallId", value);
+        }
+
+
+
+        internal static void Reset()
+        {
+            nextCallId = startCallId;
+            nextAssignmentId = startAssignmentId;
+            Clock = DateTime.Now;
+            RiskRange = TimeSpan.Zero;
+        }
+    }
+}
